a48f60b3f7b8bb1335fb82a0273aa856abd446ce
Baillie Stang <baillie.stang@gmail.com>
2022-03-06 22:22:44 -0800

    Update main.lua
    code refactor - made it nicer on the everything

--- main.lua
+++ main.lua
@@ -1,58 +1,57 @@
 -- code by puzzelism 2022
 
-function love.load()
-    -- important variables
-    SPLIT_FACTOR = 50;
-    SIN_SCALE = 20;
-    MOVE_FACTOR = -1;
-    VERSION = '0.0.1';
-    
-    -- point arrays
-    points = {};
-    points2 = {};
+-- shortcut ops
+lg = love.graphics;
+lk = love.keyboard;
 
-    idk = 0.1;
-end
+VERSION = '0.0.1';
 
 -- base config
-shadow = true;
-wireframe = false;
-cosine = false;
-menu = true;
-move = 1;
+SHADOW = true;
+WIREFRAME = false;
+COSINE = false;
+MENU = true;
 
 -- colors
+bgColor = {0.05, 0.05, 0.05}
 sinColor = {1, 1, 1}
 cosColor = {1, 0.5, 0.5}
 
+function love.load()
+    -- important variables
+    SPLIT_FACTOR = 50;
+    SIN_SCALE = 30;
+    MOVE_FACTOR = 1;
+    FREQ = 0.25;
+    move = MOVE_FACTOR;
+    
+    -- init arrays
+    points = {};
+    points2 = {};
+end
+
 function love.update(dt)
-    if(love.keyboard.isDown("w"))then
-        SPLIT_FACTOR = SPLIT_FACTOR + 10
-    elseif(love.keyboard.isDown("s"))then
-        SPLIT_FACTOR = SPLIT_FACTOR - 10
+    if(lk.isDown("up"))then
+        SIN_SCALE = SIN_SCALE + 5;
+    elseif(lk.isDown("down"))then
+        SIN_SCALE = SIN_SCALE - 5;
+    end
+    if(lk.isDown("right"))then
+        FREQ = FREQ +(0.05 * dt);
+    elseif(lk.isDown("left"))then
+        FREQ = FREQ - (0.05 * dt);
     end
 
-    w = love.graphics.getWidth();
-    h = love.graphics.getHeight();
+    w = lg.getWidth();
+    h = lg.getHeight();
     sliceW = w / SPLIT_FACTOR;
     move = move + (MOVE_FACTOR * dt);
 
     for x = 0,SPLIT_FACTOR do
-        sined = 20+(math.sin(x*idk+move)*SIN_SCALE)
-        cosined = 20+(math.cos(x*idk-move)*SIN_SCALE)
-        points[x] = (love.graphics.getHeight()/4)-20 + sined;
-        points2[x] = (love.graphics.getHeight()/4)-20 + cosined;
-    end
-
-    if(love.keyboard.isDown("up"))then
-        SIN_SCALE = SIN_SCALE + 5;
-    elseif(love.keyboard.isDown("down"))then
-        SIN_SCALE = SIN_SCALE - 5;
-    end
-    if(love.keyboard.isDown("right"))then
-        idk = idk +(0.05 * dt);
-    elseif(love.keyboard.isDown("left"))then
-        idk = idk - (0.05 * dt);
+        sined = 20+(math.sin(x*FREQ+move)*SIN_SCALE)
+        cosined = 20+(math.cos(x*FREQ-move)*SIN_SCALE)
+        points[x] = (lg.getHeight()/4)-20 + sined;
+        points2[x] = (lg.getHeight()/4)-20 + cosined;
     end
 end
 
@@ -60,52 +59,57 @@ function love.keypressed(key)
     if(key == "escape")then
         love.event.quit()
     elseif(key == "space")then
-        shadow = not shadow
+        SHADOW = not SHADOW
     elseif(key == "=")then 
         MOVE_FACTOR = MOVE_FACTOR + 1;
     elseif(key == "-")then
         MOVE_FACTOR = MOVE_FACTOR - 1;
     elseif(key == "lshift")then
-        wireframe = not wireframe
+        WIREFRAME = not WIREFRAME
     elseif(key == "tab")then
-        cosine = not cosine
+        COSINE = not COSINE
     elseif(key == "lalt")then
-        menu = not menu
+        MENU = not MENU
+    elseif(key == "w")then
+        SPLIT_FACTOR = SPLIT_FACTOR + 10;
+    elseif(KEY == "S")then
+        SPLIT_FACTOR = SPLIT_FACTOR - 10;
     end
 end
 
 function love.draw()
-    love.graphics.scale(2, 2);
-    love.graphics.setBackgroundColor(0.05, 0.05, 0.05);
+    -- graphcis step
+    lg.scale(2, 2);
+    lg.setBackgroundColor(bgColor);
 
-    if(menu)then
-        love.graphics.setColor(1, 1, 1);
-        love.graphics.print("[UP/DOWN] SCALE: "..SIN_SCALE,0,0);
-        love.graphics.print("[+/-] MOVE: "..MOVE_FACTOR,0,16)
-        love.graphics.print("[<-/->] FREQ: "..idk,0,32);
-        love.graphics.print("[SPACE] SHADOW: "..(shadow and 'HI' or 'LO'),0,48);
-        love.graphics.print("[LSHIFT] WIRE: "..(wireframe and ('['..(cosine and SPLIT_FACTOR*2 or SPLIT_FACTOR)..' NODES]') or 'LO'), 0, 64)
-        love.graphics.print("[TAB] VIEW_COS: "..(cosine and 'HI' or 'LO'), 0, 80)
-        love.graphics.print(love.timer.getFPS().."fps", w/2-40, 16)
-        love.graphics.setColor(1,1,0);
-        love.graphics.print("v"..VERSION, w/4-20, h/2-40)
-        love.graphics.setColor(0.5,0.5,0.5);
-        love.graphics.print("made by STANG 2022", w/4-64, h/2-16)
+    -- menu render
+    if(MENU)then
+        lg.setColor(1, 1, 1);
+        lg.print("[UP/DOWN] SCALE: "..SIN_SCALE,0,0);
+        lg.print("[+/-] MOVE: "..MOVE_FACTOR,0,16)
+        lg.print("[<-/->] FREQ: "..FREQ,0,32);
+        lg.print("[SPACE] SHADOW: "..(SHADOW and 'HI' or 'LO'),0,48);
+        lg.print("[LSHIFT] WIRE: "..(WIREFRAME and ('['..(cosine and SPLIT_FACTOR*2 or SPLIT_FACTOR)..' NODES]') or 'LO'), 0, 64)
+        lg.print("[TAB] VIEW_COS: "..(COSINE and 'HI' or 'LO'), 0, 80)
+        lg.print(love.timer.getFPS().."fps", w/2-40, 16)
+        lg.setColor(1,1,0);
+        lg.print("v"..VERSION, w/4-20, h/2-40)
+        lg.setColor(0.5,0.5,0.5);
+        lg.print("made by puzzel 2022", w/4-64, h/2-16)
     end
-
-    love.graphics.print("MENU [ALT]", w/2-74, 0)
+    lg.print("MENU [ALT]", w/2-74, 0)
     
-    -- RENDER
+    -- wave render
     for i = 0,SPLIT_FACTOR do
-        if(cosine)then
-            love.graphics.setColor(cosColor)
-            love.graphics.rectangle((wireframe and 'line' or 'fill'), i*sliceW, points2[i], sliceW, 40);
+        if(COSINE)then
+            lg.setColor(cosColor)
+            lg.rectangle((WIREFRAME and 'line' or 'fill'), i*sliceW, points2[i], sliceW, 40);
         end
-        if(shadow)then
-            love.graphics.setColor(0.5, 0.5, 0.5);
-            love.graphics.rectangle("fill", i*sliceW+4, points[i]+4, sliceW, 40);
+        if(SHADOW)then
+            lg.setColor(0.5, 0.5, 0.5);
+            lg.rectangle("fill", i*sliceW+4, points[i]+4, sliceW, 40);
         end
-        love.graphics.setColor(sinColor);
-        love.graphics.rectangle((wireframe and 'line' or 'fill'), i*sliceW, points[i], sliceW, 40);
+        lg.setColor(sinColor);
+        lg.rectangle((WIREFRAME and 'line' or 'fill'), i*sliceW, points[i], sliceW, 40);
     end
 end
