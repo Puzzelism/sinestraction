9d7deab1e3c3c8abf372409708a24453d500298c
Baillie Stang <baillie.stang@gmail.com>
2022-03-07 16:54:14 -0800

    v0.1.2
    Added title screen and began work on a new framework which will make future additions better

--- main.lua
+++ main.lua
@@ -52,8 +52,9 @@ function reload()
     MOVE_FACTOR = 1;
     FREQ = 0.25;
     HEIGHT = 40;
-    AUDIO_TIMER_MAX = 20;
+    AUDIO_TIMER_MAX = 25;
     OFFSET = 20;
+    TITLE = true;
     AUDIO_TIMER = AUDIO_TIMER_MAX;
     move = MOVE_FACTOR;
 
@@ -77,6 +78,9 @@ function reload()
         la.newSource(select3, static),
     }
 
+    -- graphics
+    titleScreen = lg.newImage("res/title.png")
+
     -- init arrays
     points = {};
     points2 = {};
@@ -91,7 +95,6 @@ function playSelect(sound)
         end
         AUDIO_TIMER = AUDIO_TIMER_MAX
     end
-    print(soundTick);
 end
 
 function love.load()
@@ -101,20 +104,22 @@ end
 
 function love.update(dt)
     -- scaling
-    if(lk.isDown("up"))then
-        SIN_SCALE = SIN_SCALE + 5;
-        playSelect(synthSnd)
-    elseif(lk.isDown("down"))then
-        SIN_SCALE = SIN_SCALE - 5;
-        playSelect(synthSnd)
-    end
-    -- frequency
-    if(lk.isDown("right"))then
-        FREQ = FREQ +(0.005);
-        playSelect(synthSnd)
-    elseif(lk.isDown("left"))then
-        FREQ = FREQ - (0.005);
-        playSelect(synthSnd)
+    if(not TITLE)then
+        if(lk.isDown("up"))then
+            SIN_SCALE = SIN_SCALE + 5;
+            playSelect(synthSnd)
+        elseif(lk.isDown("down"))then
+            SIN_SCALE = SIN_SCALE - 5;
+            playSelect(synthSnd)
+        end
+        -- frequency
+        if(lk.isDown("right"))then
+            FREQ = FREQ +(0.005);
+            playSelect(synthSnd)
+        elseif(lk.isDown("left"))then
+            FREQ = FREQ - (0.005);
+            playSelect(synthSnd)
+        end
     end
 
     -- get env variables
@@ -133,29 +138,52 @@ function love.update(dt)
     end
 end
 
-function love.keypressed(key)
-    playSelect(selectSnd)
-    if(key == "escape")then love.event.quit()
-    elseif(key == "space")then SHADOW = not SHADOW
-    elseif(key == "=")then  MOVE_FACTOR = MOVE_FACTOR + 1;
-    elseif(key == "-")then MOVE_FACTOR = MOVE_FACTOR - 1;
-    elseif(key == "lshift")then WIREFRAME = not WIREFRAME
-    elseif(key == "tab")then COSINE = not COSINE
-    elseif(key == "lalt")then MENU = not MENU
-    elseif(key == "w")then SPLIT_FACTOR = SPLIT_FACTOR + 10;
-    elseif(key == "s")then SPLIT_FACTOR = SPLIT_FACTOR - 10;
-    elseif(key == "return")then fullscreen = not fullscreen love.window.setFullscreen(fullscreen, fstype)
-	elseif(key == "d")then HEIGHT = HEIGHT + 10;
-    elseif(key == "a")then HEIGHT = HEIGHT - 10;
-    elseif(key == "q")then 
-    elseif(key == "1")then randomizeColors(1);
-    elseif(key == "2")then randomizeColors(2);
-    elseif(key == "3")then randomizeColors(7);
-    elseif(key == "4")then randomizeColors(3);
-    elseif(key == "5")then randomizeColors(5);
-    elseif(key == "6")then randomizeColors(6);
-    elseif(key == "r")then reload();
+function love.draw()
+    lg.setColor(fontColor)
+    lg.scale(2, 2);
+    lg.setBackgroundColor(bgColor);
+
+    drawWaves()
+    
+    if(not TITLE)then
+        -- menu render
+        if(MENU)then
+            drawMenu()
+        end
+    else
+        drawTitle()
     end
+end
+
+function love.keypressed(key)
+    if(TITLE)then
+        if(key == "escape")then love.event.quit()
+        elseif(key == "space")then TITLE = not TITLE
+        end
+    elseif(not TITLE)then
+        playSelect(selectSnd)
+        if(key == "escape")then TITLE = not TITLE
+        elseif(key == "space")then SHADOW = not SHADOW
+        elseif(key == "=")then  MOVE_FACTOR = MOVE_FACTOR + 1;
+        elseif(key == "-")then MOVE_FACTOR = MOVE_FACTOR - 1;
+        elseif(key == "lshift")then WIREFRAME = not WIREFRAME
+        elseif(key == "tab")then COSINE = not COSINE
+        elseif(key == "lalt")then MENU = not MENU
+        elseif(key == "w")then SPLIT_FACTOR = SPLIT_FACTOR + 10;
+        elseif(key == "s")then SPLIT_FACTOR = SPLIT_FACTOR - 10;
+	    elseif(key == "d")then HEIGHT = HEIGHT + 10;
+        elseif(key == "a")then HEIGHT = HEIGHT - 10;
+        elseif(key == "1")then randomizeColors(1);
+        elseif(key == "2")then randomizeColors(2);
+        elseif(key == "3")then randomizeColors(7);
+        elseif(key == "4")then randomizeColors(3);
+        elseif(key == "5")then randomizeColors(5);
+        elseif(key == "6")then randomizeColors(6);
+        elseif(key == "r")then reload();
+        end
+    end
+
+    if(key == "return")then fullscreen = not fullscreen love.window.setFullscreen(fullscreen, fstype) end
 end
 
 function love.wheelmoved(x, y)
@@ -164,42 +192,39 @@ function love.wheelmoved(x, y)
     end
 end
 
-function love.draw()
-    -- graphcis step
-    lg.setColor(fontColor)
-    lg.scale(2, 2);
-    lg.setBackgroundColor(bgColor);
+function drawMenu()
+    lg.setColor(fontColor);
+    lg.print("[UP/DOWN] SCALE: "..SIN_SCALE,0,0)
+    lg.print("[+/-] MOVE: "..MOVE_FACTOR,0,16)
+    lg.print("[<-/->] FREQ: "..FREQ,0,32)
+    lg.print("[A/D] HEIGHT: "..HEIGHT,0,48)
+    lg.print("[LSHIFT] WIRE: "..(WIREFRAME and ('['..(COSINE and SPLIT_FACTOR*2 or SPLIT_FACTOR)..' NODES, W/S to mod]') or 'LO'), 0, 64)
+    lg.print("[TAB] VIEW_COS: "..(COSINE and 'HI' or 'LO'), 0, 80)
+    lg.print("FULLSCRN [ENTER]", w/2-120, 16)
+    lg.print((SHADOW and 'HI' or 'LO').." SHADOWS [SPACE]", w/2-138, 32)
+    lg.print("COLORIZE [1-6][R RESET]", w/2-158, 48)
+    
+    lg.setColor(versionColor);
+    lg.print("v"..VERSION, w/4-20, h/2-50)
 
-    -- menu render
-    if(MENU)then
-        lg.setColor(fontColor);
-        lg.print("[UP/DOWN] SCALE: "..SIN_SCALE,0,0);
-        lg.print("[+/-] MOVE: "..MOVE_FACTOR,0,16)
-        lg.print("[<-/->] FREQ: "..FREQ,0,32);
-        lg.print("[A/D] HEIGHT: "..HEIGHT,0,48)
-        lg.print("[LSHIFT] WIRE: "..(WIREFRAME and ('['..(COSINE and SPLIT_FACTOR*2 or SPLIT_FACTOR)..' NODES, W/S to mod]') or 'LO'), 0, 64)
-        lg.print("[TAB] VIEW_COS: "..(COSINE and 'HI' or 'LO'), 0, 80)
-        lg.print("FULLSCRN [ENTER]", w/2-120, 16)
-        lg.print((SHADOW and 'HI' or 'LO').." SHADOWS [SPACE]", w/2-138, 32)
-        lg.print("COLORIZE [1-6][R RESET]", w/2-158, 48)
-        
-        lg.setColor(versionColor);
-        lg.print("v"..VERSION, w/4-20, h/2-50)
+    lg.setColor(1,1,1,1)
+    lg.printf({shadowColor, "y = ", highlightColor, OFFSET-20, shadowColor, " + SIN(x * ", highlightColor, FREQ, shadowColor, " + ", highlightColor, MOVE_FACTOR, shadowColor, ") * ", highlightColor, SIN_SCALE}, w/4-95, h/2-32, w, center)
 
-        lg.setColor(1,1,1,1)
-        lg.printf({shadowColor, "y = ", highlightColor, OFFSET-20, shadowColor, " + SIN(x * ", highlightColor, FREQ, shadowColor, " + ", highlightColor, MOVE_FACTOR, shadowColor, ") * ", highlightColor, SIN_SCALE}, w/4-95, h/2-32, w, center)
+    lg.setColor(0.5,0.5,0.5);
+    lg.print("made by puzzel 2022", w/4-64, h/2-16)
 
-        lg.setColor(0.5,0.5,0.5);
-        lg.print("made by puzzel 2022", w/4-64, h/2-16)
-    end
     lg.setColor(MENU and fontColor or shadowColor)
     lg.print("MENU [ALT]", w/2-74, 0)
     lg.print(love.timer.getFPS().."fps", w/2-40, (MENU and 64 or 16))
-    
-    local centerX = w/4
-	local centerY = h/4
+end
 
-    -- wave render
+function drawTitle()
+    lg.setBackgroundColor(bgColor);
+    titleScreen:setFilter("linear", "nearest");
+    lg.draw(titleScreen, (w/2-titleScreen:getPixelWidth())/2, (h/2-titleScreen:getPixelHeight())/2);
+end
+
+function drawWaves()
     for i = 0,SPLIT_FACTOR do
         if(COSINE)then
             lg.setColor(cosColor)
